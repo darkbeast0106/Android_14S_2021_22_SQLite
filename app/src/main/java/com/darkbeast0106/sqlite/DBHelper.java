e6ae223b4e4c4196d89d18ea2fd8106707cd4e6e
Budaházi Bence <budahazi.bence@petrik.hu>
2021-11-10 18:13:37 +0100

    Adat rögzítésének megvalósítása

---
+++ app/src/main/java/com/darkbeast0106/sqlite/DBHelper.java
@@ -0,0 +1,51 @@
+package com.darkbeast0106.sqlite;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+// https://developer.android.com/reference/android/database/sqlite/SQLiteOpenHelper
+// https://developer.android.com/reference/android/database/sqlite/SQLiteDatabase
+
+public class DBHelper extends SQLiteOpenHelper {
+    private static final String DB_NAME = "tanulok.db";
+    private static final int DB_VERSION = 1;
+
+    private static final String TABLE_NAME = "tanulok";
+    private static final String COL_ID = "id";
+    private static final String COL_VEZNEV = "vezeteknev";
+    private static final String COL_KERNEV = "keresztnevnev";
+    private static final String COL_JEGY = "jegy";
+
+    public DBHelper(Context context) {
+        super(context, DB_NAME, null, DB_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String sql = "CREATE TABLE "+TABLE_NAME +" (" +
+                COL_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                COL_VEZNEV + " TEXT NOT NULL, " +
+                COL_KERNEV + " TEXT NOT NULL, " +
+                COL_JEGY + " INTEGER NOT NULL " +
+                ");";
+        db.execSQL(sql);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS "+TABLE_NAME);
+        onCreate(db);
+    }
+
+    public boolean rogzites(String vezeteknev, String keresztnev, int jegy){
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues values = new ContentValues();
+        values.put(COL_VEZNEV, vezeteknev);
+        values.put(COL_KERNEV, keresztnev);
+        values.put(COL_JEGY, jegy);
+        return db.insert(TABLE_NAME, null, values) != -1;
+    }
+
+}
--- app/src/main/java/com/darkbeast0106/sqlite/RogzitesActivity.java
+++ app/src/main/java/com/darkbeast0106/sqlite/RogzitesActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class RogzitesActivity extends AppCompatActivity {
     private Button btnRogzit;
@@ -14,6 +15,7 @@ public class RogzitesActivity extends AppCompatActivity {
     private EditText editVezNev;
     private EditText editKerNev;
     private EditText editJegy;
+    private DBHelper adatbazis;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +30,38 @@ public class RogzitesActivity extends AppCompatActivity {
                 finish();
             }
         });
+        btnRogzit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String vezeteknev = editVezNev.getText().toString().trim();
+                String keresztnev = editKerNev.getText().toString().trim();
+                String jegyString = editJegy.getText().toString().trim();
+                if (vezeteknev.isEmpty() || keresztnev.isEmpty() || jegyString.isEmpty()){
+                    Toast.makeText(getApplicationContext(), "Minden mező kitöltése kötelező",
+                            Toast.LENGTH_SHORT).show();
+                } else{
+                    try {
+                        int jegy = Integer.parseInt(jegyString);
+                        if (jegy < 1 || jegy > 5){
+                            Toast.makeText(getApplicationContext(),
+                                    "A jegynek 1 és 5 közötti számnak kell lennie",
+                                    Toast.LENGTH_SHORT).show();
+                        } else{
+                            if (adatbazis.rogzites(vezeteknev, keresztnev, jegy)){
+                                Toast.makeText(getApplicationContext(), "Sikeres rögzítés",
+                                        Toast.LENGTH_SHORT).show();
+                            } else{
+                                Toast.makeText(getApplicationContext(), "Sikeretelen rögzítés",
+                                        Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    } catch (NumberFormatException ex) {
+                        Toast.makeText(getApplicationContext(), "A jegynek számnak kell lennie",
+                                Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        });
     }
 
     private void init(){
@@ -36,5 +70,6 @@ public class RogzitesActivity extends AppCompatActivity {
         editVezNev = findViewById(R.id.edit_vnev);
         editKerNev = findViewById(R.id.edit_knev);
         editJegy = findViewById(R.id.edit_jegy);
+        adatbazis = new DBHelper(this);
     }
 }
