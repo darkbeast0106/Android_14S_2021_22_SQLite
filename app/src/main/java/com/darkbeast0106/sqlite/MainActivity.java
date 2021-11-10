f05dec638a588ce5b0278b4294be17066aef43c3
Budaházi Bence <budahazi.bence@petrik.hu>
2021-11-10 17:20:30 +0100

    MainActivity.java  - init()  - Rogzitesre átlépés

--- app/src/main/java/com/darkbeast0106/sqlite/MainActivity.java
+++ app/src/main/java/com/darkbeast0106/sqlite/MainActivity.java
@@ -2,13 +2,40 @@ package com.darkbeast0106.sqlite;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private Button btnListaz;
+    private Button btnRogzitesre;
+    private Button btnModositasra;
+    private Button btnTorlesre;
+    private TextView textLista;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        init();
+        btnRogzitesre.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent rogzitesre = new Intent(MainActivity.this, RogzitesActivity.class);
+                startActivity(rogzitesre);
+                finish();
+            }
+        });
+    }
+
+    private void init(){
+        btnListaz = findViewById(R.id.btn_olvasas);
+        btnRogzitesre = findViewById(R.id.btn_rogzitesre);
+        btnModositasra = findViewById(R.id.btn_modositasra);
+        btnTorlesre = findViewById(R.id.btn_torlesre);
+        textLista = findViewById(R.id.text_lista);
     }
 }
