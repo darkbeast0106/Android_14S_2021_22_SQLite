6d048254cec7e57f65e542a4a1291b702c5454e0
Budaházi Bence <budahazi.bence@petrik.hu>
2021-11-17 17:21:58 +0100

    Adatok listája görgethető.

--- app/src/main/java/com/darkbeast0106/sqlite/MainActivity.java
+++ app/src/main/java/com/darkbeast0106/sqlite/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.database.Cursor;
 import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -24,6 +25,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         init();
+
+        textLista.setMovementMethod(new ScrollingMovementMethod());
         btnRogzitesre.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
